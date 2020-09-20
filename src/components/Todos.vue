137af006c42574ad2a6f0172ef1148496a6fa26c
Yoan Hristov <ioan.xristov@abv.bg>
2020-09-20 22:14:56 +0300

    some 30 mins coding here twat

---
+++ src/components/Todos.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div v-bind:key="todo" v-for="todo in todos">
+      <TodoItem v-bind:todo="todo" />
+    </div>
+  </div>
+</template>>
+<script>
+import TodoItem from './TodoItem.vue'
+export default {
+  name: "Todos",
+  components: {
+      TodoItem
+  },
+  props: ["todos"]
+}
+</script>>
+<style lang="stylus" scoped>
+
+</style>
